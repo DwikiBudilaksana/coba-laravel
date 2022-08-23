39bcce99d0394a80e1cd834baa2fa2d7cfcdae18
DwikiBudilaksana <made.laksana@traveloka.com>
2022-08-23 14:33:01 +0700

    [DC1-11] N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,15 @@ Route::get('/category', function(){
 });
 
 Route::get('/category/{category:slug}', function(Category $category){
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
     ]);
 });
 
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
-        'title' => 'User Post',
-        'posts' => $author->posts
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });

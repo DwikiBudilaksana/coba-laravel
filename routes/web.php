a759bf9b01330c0114390f034127ca6c954d165a
DwikiBudilaksana <made.laksana@traveloka.com>
2022-09-11 11:16:02 +0700

    [DC1-14] View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use App\Models\Category;
 use App\Models\User;
-use Illuminate\Routing\Route as RoutingRoute;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
 Route::get('/', function () {
@@ -51,3 +53,6 @@ Route::get('/authors/{author:username}', function(User $author){
         'posts' => $author->posts->load('category', 'author')
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
